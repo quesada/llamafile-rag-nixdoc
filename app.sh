8c342b33e856aeeea6a725dbe39feb4a01aaebf7
Jose Quesada <me@josequesada.com>
2024-10-24 19:44:36 +0000

    adding data; TODO remove as this should not be public

--- app.sh
+++ app.sh
@@ -11,10 +11,11 @@ echo "Starting llamafile servers..."
 
 "models/embedding_model.llamafile" \
 --server \
+-ngl 9999 \
 --nobrowser \
 --port "${EMBEDDING_MODEL_PORT}" > logs/embedding_model.log 2>&1 &
 pid=$!
-sleep 20
+sleep 5
 err=$?
 if [ "${err}" -ne 0 ]; then
   echo "Failed to start embedding model llamafile"
@@ -23,13 +24,14 @@ fi
 echo "${pid}" > .pid_embedding_model
 echo "started embedding model"
 
-
+date
 "models/generation_model.llamafile" \
 --server \
+-ngl 9999 \
 --nobrowser \
 --port "${GENERATION_MODEL_PORT}" > logs/generation_model.log 2>&1 &
 pid=$!
-sleep 20
+sleep 5
 err=$?
 if [ "${err}" -ne 0 ]; then
   echo "Failed to start generation model llamafile"
@@ -37,6 +39,7 @@ if [ "${err}" -ne 0 ]; then
 fi
 echo "${pid}" > .pid_generation_model
 echo "started generation model"
+date
 
 # Run RAG app
 python app.py "$@"
